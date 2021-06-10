f49d1dc27de9fac4c5c8930c3bdeffd5c7c13b75
Kirill Petrov <kirill@MacBook-Pro-Kirill.local>
2021-06-10 21:01:44 +0300

    data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Route } from "react-router"
 import { Header } from "./components"
 import { Home, Cart } from "./pages"
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+      fetch('db.json')
+      .then((res) => res.json())
+      .then(json => {
+        setPizzas(json.pizzas)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
