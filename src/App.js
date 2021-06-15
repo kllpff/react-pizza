bb2d6eafe96862ccbfdcc0086aea17e8e7fa70f7
Kirill Petrov <kirill@MacBook-Pro-Kirill.local>
2021-06-15 23:27:33 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, Component } from "react"
 import { Route } from "react-router"
 import { Header } from "./components"
 import { Home, Cart } from "./pages"
 import axios from 'axios'
+import { connect } from 'react-redux'
+import store from './redux/store'
+import { setPizzas } from './redux/actions/pizzas'
 
-function App() {
-  const [pizzas, setPizzas] = useState([])
+// function App() {
+//   useEffect(() => {
+//       axios.get('db.json').then(({data}) => {
+//         setPizzas(data.pizzas)
+//       })
+//   }, [])
 
-  useEffect(() => {
-      axios.get('db.json').then(({data}) => {
-        setPizzas(data.pizzas)
-      })
-  }, [])
+//   return ;
+// }
 
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Route path="/" render={() => <Home items={pizzas } />} exact />
-        <Route path="/cart" component={Cart} exact />
+class App extends Component {
+
+  componentDidMount() {
+    axios.get('db.json').then(({data}) => {
+      this.props.setPizzas(data.pizzas)
+    })
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route path="/" render={() => <Home items={this.props.items} />} exact />
+          <Route path="/cart" component={Cart} exact />
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+    filters: state.filters
+  }
+}
+
+const mapDispatchToProps = {
+  setPizzas
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -4,7 +4,7 @@ export default function SortPopup({ items }) {
   const [visiblePopup, setVisiblePopup] = useState(false)
   const [activeItem, setActiveItem] = useState(0)
   const sortRef = useRef()
-  const activeLabel = items[activeItem]
+  const activeLabel = items[activeItem].name
 
   const onSelectItem = (index) => {
     setActiveItem(index)
@@ -49,12 +49,12 @@ export default function SortPopup({ items }) {
         <div className="sort__popup">
           <ul>
             {items &&
-              items.map((name, index) => (
+              items.map((obj, index) => (
                 <li
                   className={activeItem === index ? 'active' : ''}
                   onClick={() => onSelectItem(index)}
-                  key={`${name}_${index}`}>
-                  {name}
+                  key={`${obj.type}_${index}`}>
+                  {obj.name}
                 </li>
               ))}
           </ul>
